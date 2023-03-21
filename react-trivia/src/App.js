00b581d5923a3c1d7b5c29d0a0ff7c9869c5cfeb
GerardoRoman <offthegrd3657@gmail.com>
2023-03-20 22:59:56 -0400

    created the layout for the trivia game, aka how it's going to look, about to start working on getting categories to show up

---
+++ react-trivia/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+// import { useState } from 'react';
+// import Axios from 'axios';
+
+// const API_URL = 'https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple';
+
+function App() {
+  return (
+    
+    <div className="container">
+      <div className='question'>
+        <h1>this is a question</h1>
+      </div>
+    <div className='all-buttons'>
+      <button className='button1'>button1</button>
+      <button className='button3'>button3</button>
+      <button className='button2'>button2</button>
+      <button className='button4'>button4</button>
+    </div>
+      {/* 1. header */}
+    
+      {/* 2. curent score */}
+
+      {/* 3.question card */}
+
+      {/* 4. final results */}
+
+    </div>
+  );
+}
+
+export default App;
