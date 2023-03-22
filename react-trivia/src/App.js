e6cafb0ea8f696408dd1e57fb55eaa87df0767b0
GerardoRoman <offthegrd3657@gmail.com>
2023-03-22 15:12:22 -0400

    able to select category, gotta get triviaGame.js to render now with questions

--- react-trivia/src/App.js
+++ react-trivia/src/App.js
@@ -1,34 +1,37 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import Game from './triviaGame'
 
 function App() {
   const [categories, setCategories] = useState(null)
+  const [selectedCategory, setSelectedCategory] = useState(null)
+  // const [question, setQuestion] = useState(null)
 
   useEffect(() => {
     const API_URL = 'https://opentdb.com/api_category.php';
 
     axios.get(API_URL).then((taco) => setCategories(taco.data.trivia_categories) )
     }, [])
+
+    const handleCategorySelect = (categoryId) => {
+      setSelectedCategory(categoryId);
+      // continue to the next phase of the triva game using the selected category ID
+    };
+if(selectedCategory) {
+  return (
+    < Game categoryId={selectedCategory} />
+  )
+}
   return (
 
     <div className='category-list'>
+      <h1>Select a category!</h1>
       {
-      categories && categories.map(category => <button>{category.name}</button>)
-      }
+      categories && categories.map((category) => (
+      <button key={category.id} onClick={() => handleCategorySelect(category.id)}>{category.name}</button>
+      ))}
     </div>
-    // <div className="container">
-    //   <div className='question'>
-    //     <h1>this is a question</h1>
-    //   </div>
-    // <div className='all-buttons'>
-    //   <button className='button1'>button1</button>
-    //   <button className='button3'>button3</button>
-    //   <button className='button2'>button2</button>
-    //   <button className='button4'>button4</button>
-    // </div>
-    // </div>
   );
 }
 
---
+++ react-trivia/src/triviaGame.js
@@ -0,0 +1,16 @@
+export default function Game(categoryId) {
+
+ }
+
+ // <div className="container">
+    //   <div className='question'>
+    //     <h1>this is a question</h1>
+    //   </div>
+    // <div className='all-buttons'>
+    //   <button className='button1'>button1</button>
+    //   <button className='button3'>button3</button>
+    //   <button className='button2'>button2</button>
+    //   <button className='button4'>button4</button>
+    // </div>
+    // </div>
+
