54c59c95e55be5d3a799f3cf560ffb93504177a4
GerardoRoman <offthegrd3657@gmail.com>
2023-03-23 01:11:16 -0400

    i can 't remember what i commited last but now i am at the point where i am having too many of the same answer choices when all i need is 4. too many duplicates

--- react-trivia/src/triviaGame.js
+++ react-trivia/src/triviaGame.js
@@ -1,32 +1,44 @@
 import axios from "axios";
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 
 export default function Game({categoryId}) {
     const [questions, setQuestions] = useState(null)
     const [currentQuestion, setCurrentQuestion] = useState(0)
-let question = useRef(null)
+    const answers = useRef(null)
+    let question = useRef(null)
+
     useEffect(() => {
         const URL = `https://opentdb.com/api.php?amount=10&category=${categoryId}`;
         axios.get(URL).then((response) => { 
-            // console.log(URL)
+
             setQuestions(response.data)
-            question.current=response.data.results[0].question
-            // console.log(categoryId)
+            question.current = response.data.results[0].question
+            let correctAnswer = response.data.results[0].correct_answer
+            let wrongAnswers = response.data.results[0].incorrect_answers
+            answers.current = [...wrongAnswers, correctAnswer]
+            // score.current = 0
+            console.log(answers.current)
 })
-    }, [])
+}, [])
+
     return (
+        <>
  <div className="container">
       <div className='question'>
         <h1>{question.current}</h1>
       </div>
-    <div className='all-buttons'>
-      <button className='button1'>{question.current}</button>
-      <button className='button3'>{question.current}</button>
-      <button className='button2'>{question.current}</button>
-      <button className='button4'>{question.current}</button>
+    <div className='all-buttons'>{answers.current && answers.current.map((answer) => (
+        <div>
+      <button className='button1'>{answer}</button>
+      <button className='button3'>{answer}</button>
+      <button className='button2'>{answer}</button>
+      <button className='button4'>{answer}</button> 
+      </div>
+      ))}
     </div>
-    </div>
+</div>
+</>
     )
  }
 
