44c414884cff08af74de22235cbf687f045f5670
GerardoRoman <offthegrd3657@gmail.com>
2023-03-24 12:38:16 -0400

    added an alert to show when a user gets a question wrong, but haven't figured out how to progress onto the next question regardless of whether it was answered correctly or not

--- react-trivia/src/triviaGame.js
+++ react-trivia/src/triviaGame.js
@@ -7,6 +7,7 @@ export default function Game({categoryId}) {
     const [currentQuestion, setCurrentQuestion] = useState(0)
     const answers = useRef(null)
     let question = useRef(null)
+    let score = useRef(0)
 
     useEffect(() => {
         const URL = `https://opentdb.com/api.php?amount=10&category=${categoryId}`;
@@ -17,10 +18,20 @@ export default function Game({categoryId}) {
             let correctAnswer = response.data.results[0].correct_answer
             let wrongAnswers = response.data.results[0].incorrect_answers
             answers.current = [...wrongAnswers, correctAnswer]
-            // score.current = 0
-            console.log(answers.current)
+            score.current = 0
+            // console.log(answers.current)
 })
 }, [])
+
+        function handleClick(selectedAnswer) {
+            if (questions.results[currentQuestion].correct_answer === selectedAnswer) {
+                score.current += 1
+                setCurrentQuestion(currentQuestion => currentQuestion + 1)
+            } else {
+                alert('Wrong answer!')
+                score.current -= 1
+            }
+        }
 
     return (
         <>
@@ -28,9 +39,12 @@ export default function Game({categoryId}) {
     <div className='question'>
         <h1>{question.current}</h1>
     </div>
-    <div className='all-buttons'>{answers.current && answers.current.map((answers) => (
-        <div>
-            <button key={answers} onClick={() => (answers)}>{answers}</button>
+    <div className='score'>
+        <h2>{score.current}</h2>
+    </div>
+    <div className='all-buttons'>{answers.current && answers.current.map((answer) => (
+        <div key={answer}>
+            <button onClick={() => handleClick(answer)}>{answer}</button>
         </div>
         ))}
     </div>
